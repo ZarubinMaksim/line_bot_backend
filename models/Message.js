b0787c31badf96f0910a28fd375f49f38fd9f3f2
Максим <maksim@MacBook-Neo-Maksim.local>
2026-05-17 12:49:23 +0700

    Post reply  /api/orders

---
+++ models/Message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  lineMessageId: String,
+  orderId: mongoose.Schema.Types.ObjectId,
+  text: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
